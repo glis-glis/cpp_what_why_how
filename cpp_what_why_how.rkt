bb6e77d4ae0743f23e9542578ff4829a6ecec4a5
Andreas <andreas.fueglistaler@protonmail.com>
2022-04-13 21:41:54 +0200

    first slide

---
+++ cpp_what_why_how.rkt
@@ -0,0 +1,16 @@
+#lang slideshow
+
+(require slideshow/code)
+(current-main-font "comfortaa")
+(current-title-color "green")
+(slide
+ #:title "C++"
+ 
+ 'alts
+ (list (list (item (tt "What?"))
+ (item (tt "Why?"))
+ (item (tt "How?")))
+   
+ (list (item (tt "What") "is it")
+ (item (tt "Why") "use it")
+ (item (tt "How") "to use it"))))
