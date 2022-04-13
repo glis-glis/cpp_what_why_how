cf0224d18591836210c4e3d84f1500dc47c1e79a
Andreas <andreas.fueglistaler@protonmail.com>
2022-04-13 22:29:16 +0200

    adding fantasque sans

--- cpp_what_why_how.rkt
+++ cpp_what_why_how.rkt
@@ -1,16 +1,28 @@
 #lang slideshow
 
 (require slideshow/code)
+
+(define (tt str)
+   (text str (cons 'bold (current-code-font)) (current-font-size)))
+
 (current-main-font "comfortaa")
-(current-title-color "green")
+(current-code-font "Fantasque Sans Mono")
+(current-title-color "darkgreen")
+
 (slide
  #:title "C++"
  
  'alts
- (list (list (item (tt "What?"))
- (item (tt "Why?"))
- (item (tt "How?")))
+ (list (list (item (bt "What?"))
+ (item (bt "Why?"))
+ (item (bt "How?")))
    
- (list (item (tt "What") "is it")
- (item (tt "Why") "use it")
- (item (tt "How") "to use it"))))
+ (list (para (bt "What") "is it")
+ (para (bt "Why") "use it")
+ (para (bt "How") "to use it"))))
+
+(slide
+ #:title "Next"
+ (t "huhu") (tt "gaga")
+ (code mono)
+ )
